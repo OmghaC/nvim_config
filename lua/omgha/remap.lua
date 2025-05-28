c0d4a7f71194d412dae7b85c1f0ae3153f82c817
Omgha <EternalOmgha@gmail.com>
2025-05-28 17:17:17 +0200

    update

--- lua/omgha/remap.lua
+++ lua/omgha/remap.lua
@@ -1,3 +1,9 @@
 -- Clear highlights on search when pressing <Esc> in normal mode
 --  See `:help hlsearch`
-vim.keymap.set('n','<Esc>', '<cmd>nohlsearch<CR>')
+vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
+
+-- Create file in current directory with name
+vim.keymap.set("n", "<leader>CF", function()
+	local fileName = vim.fn.input("Enter file name: ")
+	vim.cmd("!touch " .. fileName)
+end, { desc = "[C]reate [File]" })
